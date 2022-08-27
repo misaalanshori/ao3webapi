71969ef9d5e88ce39d1c05158ced3fbd78e57af9
M Isa Al Anshori <isabungsu@gmail.com>
2022-08-27 15:13:22 +0700

    Configure ports, create start script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { fetchBuilder, MemoryCache } from 'node-fetch-cache';
 const fetch = fetchBuilder.withCache(new MemoryCache({ttl: 60000}));
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Usage: \n<br/> /works/:id \n<br/> /works/:id/download/:type')
