e9c1f6c046f8651f2592bf71d0ea6dffa26f7fa6
M Isa Al Anshori <isabungsu@gmail.com>
2022-08-28 20:02:26 +0700

    added /feed/:path/:page and some fixes in others

--- README.md
+++ README.md
@@ -9,8 +9,42 @@ Functions that have been implemented
 
 Returns a JSON with all the information of the work
 
+#### Example Usage
+- http://localhost:3000/works/26301859
+- http://localhost:3000/works/25830331
+
+---
+
 ### - /works/:id/download/:type
 - **:id -> ID of the work**
 - **:type -> Filetype for the document (AZW3, EPUB, MOBI, PDF, HTML)**
 
 Returns a document file for the requested work
+
+#### Example Usage
+- http://localhost:3000/works/26301859/download/pdf
+- http://localhost:3000/works/25830331/download/mobi
+
+---
+
+### - /feed/:path/:page
+- **:path -> url encoded path and search parameters**
+- **:page -> page number**
+
+Returns the full URL, page count, and a list of works in the current page
+Examples of the paths are:
+- collections/the_owl_house/works
+- series/2149602
+- users/EleenaDume/pseuds/EleenaDume/works
+- tags/The%20Owl%20House%20(Cartoon)/works
+- tags/Amity%20Blight*s*Luz%20Noceda/works
+- works?commit=Sort+and+Filter&work_search%5Bsort_column%5D=kudos_count&tag_id=Amity+Blight*s*Luz+Noceda
+The path needs to be url encoded
+
+#### Example Usage
+- http://localhost:3000/feed/collections%2Fthe_owl_house%2Fworks/1
+- http://localhost:3000/feed/series%2F2149602/1
+- http://localhost:3000/feed/users%2FEleenaDume%2Fpseuds%2FEleenaDume%2Fworks/1
+- http://localhost:3000/feed/tags%2FThe%2520Owl%2520House%2520%28Cartoon%29%2Fworks/2
+- http://localhost:3000/feed/tags%2FAmity%2520Blight%2As%2ALuz%2520Noceda%2Fworks/3
+- http://localhost:3000/feed/works%3Fcommit%3DSort%2Band%2BFilter%26work_search%255Bsort_column%255D%3Dkudos_count%26tag_id%3DAmity%2BBlight%2As%2ALuz%2BNoceda/1
--- index.js
+++ index.js
@@ -3,16 +3,24 @@ import cors from 'cors'
 import { parse } from 'node-html-parser'
 import fetch from 'node-fetch'
 
+const ao3url = "https://archiveofourown.org/"
+const ao3downurl = "https://download.archiveofourown.org/"
+
 const app = express()
 const port = process.env.PORT || 3000
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Usage: \n<br/> /works/:id \n<br/> /works/:id/download/:type')
-})
+    res.send(`
+Usage: </br>
+/works/:id </br>
+/works/:id/download/:type </br>
+/feed/:path/:page </br>
+`)})
 
+// Get download links for document files
 function getDownloadLinks(id) {
-    fetch(`https://archiveofourown.org/works/${req.params.id}?view_adult=true`).then(async resp => {
+    fetch(`${ao3url}works/${req.params.id}?view_adult=true`).then(async resp => {
         if (resp.status != 200) {
             return null
         } else {
@@ -26,43 +34,84 @@ function getDownloadLinks(id) {
     })
 }
 
+// Parses "li.work.blurb.group" elements
+function parseWorkItem(element) {
+    let values = {
+        id: element.attributes.id.split("_")[1],
+        name: element.querySelector("h4.heading > a").textContent.trim(),
+        href: element.querySelector("h4.heading > a").attributes.href,
+        symbols: element.querySelectorAll("a.help.symbol.question > span").map(e => e.classNames),
+        tags: {}
+    }
+    element.querySelectorAll("ul.tags.commas > li").forEach(e => {
+        let content = e.querySelector("a.tag")
+        let type = e.classList.values().next().value
+        if (!values.tags[type]) {
+            values.tags[type] = []
+        }
+        values.tags[type].push({
+            text: content.textContent.trim(),
+            href: content.attributes.href
+        })
+    })
+    let series = element.querySelector("ul.series")
+    if (series) {
+        values["series"] = {
+            name: series.querySelector("li > a").textContent.trim(),
+            href: series.querySelector("li > a").attributes.href,
+            part: series.querySelector("li > strong").textContent.trim(),
+        }
+    }
+    let summary = element.querySelector("blockquote.userstuff.summary")
+    if (summary) {
+        values["summary"] = summary.structuredText.trim()
+    }
+    return values
+}
+
 // Get Work Information
 app.get("/works/:id",  (req, res) => {
-    fetch(`https://archiveofourown.org/works/${req.params.id}?view_adult=true`).then(async resp => {
+    // - :id -> ID of the work
+    // Returns a JSON with all the information of the work
+    fetch(`${ao3url}works/${req.params.id}?view_adult=true`).then(async resp => {
         if (resp.status != 200) {
-            res.sendStatus("404")
+            res.sendStatus(404)
         } else {
             let html = parse(await resp.text())
-            let infokey = html.querySelectorAll("dl.work.meta.group > dt")
-            let infocontent = html.querySelectorAll("dl.work.meta.group > dd")
             let metadata = {}
-            metadata["Title"] = html.querySelector("h2.title.heading").textContent.trim()
-            metadata["Author"] = html.querySelectorAll("h3.byline.heading > a").map(e => {return {"text": e.textContent, "href": e.attributes.href}})
-            metadata["Summary"] = html.querySelector("div.summary.module > blockquote.userstuff").textContent.trim()
-            infokey.forEach((val, index) => {
-                let metaKey = val.textContent.trim().slice(0, -1)
-                if (metaKey == "Language") {
-                    metadata[metaKey] = infocontent[index].textContent.trim()
-                } else if (metaKey == "Collections") {
-                    metadata[metaKey] = infocontent[index].querySelectorAll("a").map(e => {return {"text": e.textContent, "href": e.attributes.href}})
-                } else if (metaKey == "Stats") {
-                    let statskey = html.querySelectorAll("dl.stats > dt")
-                    let statscontent = html.querySelectorAll("dl.stats > dd")
+            metadata["title"] = html.querySelector("h2.title.heading").textContent.trim()
+            metadata["author"] = html.querySelectorAll("h3.byline.heading > a").map(e => {return {"text": e.textContent, "href": e.attributes.href}})
+            let summary = html.querySelector("div.summary.module > blockquote.userstuff")
+            if (summary) {
+                metadata["summary"] = summary.structuredText.trim()
+            }
+            metadata["tags"] = {}
+            html.querySelectorAll("dl.work.meta.group > dd.tags").forEach(e => {
+                metadata["tags"][e.classNames.split(" ")[0]] = e.querySelectorAll("ul > li > a").map(e => {return {text: e.textContent.trim(), href: e.attributes.href}})
+            })
+            html.querySelectorAll("dl.work.meta.group > dd:not(.tags)").forEach(val => {
+                let metaKey = val.classNames.split(" ")[0]
+                if (metaKey == "language") {
+                    metadata[metaKey] = val.textContent.trim()
+                } else if (metaKey == "collections") {
+                    metadata[metaKey] = val.querySelectorAll("a").map(e => {return {"text": e.textContent, "href": e.attributes.href}})
+                } else if (metaKey == "stats") {
+                    let statscontent = val.querySelectorAll("dl.stats > dd")
                     let stats = {}
-                    statskey.forEach((v, i) => {
-                        stats[v.textContent.trim().slice(0, -1)] = statscontent[i].textContent.trim()
+                    statscontent.forEach((v, i) => {
+                        stats[v.classNames] = statscontent[i].textContent.trim()
                     })
                     metadata[metaKey] = stats
-                } else if (metaKey == "Series") {
+                } else if (metaKey == "series") {
                     metadata[metaKey] = {
-                        name: infocontent[index].querySelector("span.position > a").textContent.trim(),
-                        href: infocontent[index].querySelector("span.position > a").attributes.href,
-                        prev: infocontent[index].querySelector("a.previous") ? infocontent[index].querySelector("a.previous").attributes.href : "",
-                        next: infocontent[index].querySelector("a.next") ? infocontent[index].querySelector("a.next").attributes.href : "",
-                        pos: infocontent[index].querySelector("span.position").text.match(/\d+/)[0]
+                        name: val.querySelector("span.position > a").textContent.trim(),
+                        href: val.querySelector("span.position > a").attributes.href,
+                        prev: val.querySelector("a.previous") ? val.querySelector("a.previous").attributes.href : "",
+                        next: val.querySelector("a.next") ? val.querySelector("a.next").attributes.href : "",
+                        pos: val.querySelector("span.position").text.match(/\d+/)[0]
                     }
                 } else {
-                    metadata[metaKey] = infocontent[index].querySelectorAll("li").map(e => {return {"text": e.textContent, "href": e.childNodes[0].attributes.href}})
+                    metadata[metaKey] = val.childNodes.map(e => e.textContent.trim())
                 }
             })
             res.json(metadata)
@@ -71,29 +120,70 @@ app.get("/works/:id",  (req, res) => {
     })
 })
 
+// Download Document Files for a Work
 app.get("/works/:id/download/:type", (req, res) => {
-    fetch(`https://archiveofourown.org/works/${req.params.id}?view_adult=true`).then(async resp => {
+    // - :id -> ID of the work
+    // - :type -> Filetype for the document (AZW3, EPUB, MOBI, PDF, HTML)
+    // Returns a document file for the requested work
+    fetch(`${ao3url}works/${req.params.id}?view_adult=true`).then(async resp => {
         if (resp.status != 200) {
-            res.sendStatus("404")
+            res.sendStatus(404)
         } else {
             let html = parse(await resp.text())
             let link = html.querySelectorAll("li.download > ul.expandable.secondary > li")
             .find(e => e.textContent.toLowerCase() == req.params.type.toLowerCase())
             if (link) {
                     let href = link.childNodes[0].attributes.href
-                    fetch("https://download.archiveofourown.org" + href).then(resp => {
-                        res.set("Content-Disposition", `attachment; filename="${href.split("/").pop().split("?")[0]}"`)
+                    fetch(ao3downurl + href).then(resp => {
+                        res.set({
+                            'content-disposition' : `attachment; filename="${href.split("/").pop().split("?")[0]}"`,
+                            'content-type': resp.headers.get('content-type'),
+                        })
                         resp.body.pipe(res)
                     })
             } else {
-                res.sendStatus("404")
-            }
+                res.sendStatus(404)
+            }   
         }
         
     })
 })
 
-
+// Get list of works from a path
+app.get("/feed/:path/:page", (req, res) => {
+    // - :url -> url encoded path and search parameters
+    // - :page -> page number
+    // Returns full URL, page count, and a list of works in the current page
+    let aourl = new URL(`${ao3url}${req.params.path}`)
+    aourl.searchParams.set("page", req.params.page)
+    fetch(aourl.href).then(async resp => {
+        if (resp.status != 200) {
+            res.sendStatus(404)
+        } else {
+            let html = parse(await resp.text())
+            let workslist = html.querySelectorAll("li.work.blurb.group")
+            let workspage = html.querySelectorAll("ol.pagination.actions > li")
+            let results = {
+                href: aourl.href,
+                pages: 0,
+                works: []
+            }
+            if (workslist) {
+                if (workspage.length > 3) {
+                    results.pages = workspage[workspage.length-2].textContent.trim()
+                } else {
+                    results.pages = 1
+                }
+                workslist.forEach(element => {
+                    results.works.push(parseWorkItem(element))
+                });
+                res.json(results)
+            } else {
+                res.sendStatus(404)
+            }
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log(`The app listening on port ${port}`)
